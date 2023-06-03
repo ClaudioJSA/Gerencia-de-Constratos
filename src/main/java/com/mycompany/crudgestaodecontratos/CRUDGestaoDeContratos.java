e3453812fe6b672456aa6ce69c949ad32fe1f299
Claudio Alcantara <claudio11jsa@gmail.com>
2023-06-02 22:46:36 -0300

    main

---
+++ src/main/java/com/mycompany/crudgestaodecontratos/CRUDGestaoDeContratos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudgestaodecontratos;
+
+/**
+ *
+ * @author Cláudio Juliano Santos Alcantara <Claudio Juliano at Cláudio Juliano>
+ */
+public class CRUDGestaoDeContratos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
